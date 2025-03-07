dbc617708f9a8068d92106d573be0c420e14babd
Robert Frank <robertfrank.work@gmail.com>
2025-03-07 21:24:10 +0100

    ?

---
+++ Website/nuxt-app/pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <h1>Welcome to the homepage</h1>
+    <AppAlert>
+      This is an auto-imported component.
+    </AppAlert>
+    <nuxt-link to="/secondpage">Go to the second page</nuxt-link>
+  </div>
+</template>
+
---
+++ Website/nuxt-app/pages/secondpage.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+      <h1>Welcome to the Sigma page</h1>
+      <AppAlert>
+        This is an auto-imported component.
+      </AppAlert>
+      <nuxt-link to="/">Go to the home page</nuxt-link>
+    </div>
+</template>
